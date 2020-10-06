d2ae20642c7bad6b0531c61a4ff0dafb3cffd9e3
AK <icecicle04@gmail.com>
2020-10-06 18:27:57 -0400

    added dependencies for app and mysql connection

---
+++ employees.js
@@ -0,0 +1,13 @@
+// dependencies
+
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// connections
+
+const connection = mysql.createConnection({
+  port: 3306,
+  user: "root",
+  password: "Braves2020",
+  database: "employee_DB",
+});
