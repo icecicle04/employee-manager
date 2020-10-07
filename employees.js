e51db222904dcbf27ae517ac9ccd542e762f4489
AK <icecicle04@gmail.com>
2020-10-07 00:19:44 -0400

    added promp for initial options

--- employees.js
+++ employees.js
@@ -2,6 +2,7 @@
 
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const { table } = require("console");
 // const express = require("express");
 // const { table } = require("employeeSeeds.sql");
 
@@ -16,15 +17,35 @@ const connection = mysql.createConnection({
   password: "Braves2020",
   database: "employees_db",
 });
-
+let query = "SELECT employee, role, department";
+console.log(query);
 connection.connect(function (err) {
   if (err) throw err;
+  runSearch();
 });
 
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: ["Add Employee", "Delete Employee"],
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Delete Employee":
+          deleteEmployee();
+          break;
+      }
+    });
+}
 // inquirer promp for CL goes here
 
 // listener
 
-// app.listen(PORT, () => {
-//   console.log(`App is running on http://localhost:${PORT}`);
-// });
+console.log(`App is running on http://localhost:${PORT}`);
