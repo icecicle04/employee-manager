1608752e2a8e28f70f9d1d44d2b443a5be5688a1
AK <icecicle04@gmail.com>
2020-10-06 21:30:26 -0400

    added employee seeds sql file from workbench

---
+++ employeeSeeds.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE employee
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+    first_name VARCHAR
+    (30),
+    last_name VARCHAR
+    (30),
+    role_id INT NOT NULL,
+    manager_id INTEGER,
+    PRIMARY KEY
+    (id)
+);
+
+    CREATE TABLE role
+    (
+        id INT REFERENCES employeee,
+        title VARCHAR (30),
+        salary DECIMAL,
+        department_id INTEGER
+    );
+
+    CREATE TABLE department
+    (
+        id INT REFERENCES employee,
+        name VARCHAR (30)
+    );
