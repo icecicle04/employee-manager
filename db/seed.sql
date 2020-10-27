cc383c2ac023e50902cda03d849cc492a7002d8e
AK <icecicle04@gmail.com>
2020-10-27 16:18:52 -0400

    added seeds for department

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,14 @@
 USE employees_db;
 
+INSERT INTO department
+    (department_name)
+
+VALUES
+    ("Technology"),
+    ("Operations"),
+    ("Services"),
+    ("Human Resources");
+
 INSERT INTO role
     (role_title)
 VALUES
